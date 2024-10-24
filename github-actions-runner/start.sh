9c774ba32465bd257e0ee35ea6c802326a116f44
Joren-vanGoethem <jorenvangoethem@hotmail.com>
2024-10-24 18:20:16 +0200

    whoops

--- github-actions-runner/start.sh
+++ github-actions-runner/start.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 REPO=$REPO
-ACCESS_TOKEN=$ACCESS_TOKEN
+TOKEN=$TOKEN
 
 # REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPO}/actions/runners/registration-token | jq .token --raw-output)
 
 cd /home/docker/actions-runner
 
-./config.sh --url https://github.com/${REPO} --token ${ACCESS_TOKEN}
+./config.sh --url https://github.com/${REPO} --token ${TOKEN}
 
 cleanup() {
     echo "Removing runner..."
-    ./config.sh remove --unattended --token ${REG_TOKEN}
+    ./config.sh remove --unattended --token ${TOKEN}
 }
 
 trap 'cleanup; exit 130' INT
